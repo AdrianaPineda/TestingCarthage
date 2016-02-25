17e8b32dc533acf3d94e7a9386b9c4fb210fafc9
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-02-25 18:59:55 -0500

    Add framework target

---
+++ TestingCarthageFramework/TestingCarthageFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestingCarthageFramework.h
+//  TestingCarthageFramework
+//
+//  Created by Adriana Pineda on 2/25/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestingCarthageFramework.
+FOUNDATION_EXPORT double TestingCarthageFrameworkVersionNumber;
+
+//! Project version string for TestingCarthageFramework.
+FOUNDATION_EXPORT const unsigned char TestingCarthageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingCarthageFramework/PublicHeader.h>
+
+#import <TestingCarthageFramework/Sum.h>
