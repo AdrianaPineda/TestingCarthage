7ab36148ac00a03acdd4cd3d9051617fddef36e6
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-02-25 16:20:20 -0500

    Initialize project

---
+++ TestingCarthage/TestingCarthage.h
@@ -0,0 +1,13 @@
+//
+//  TestingCarthage.h
+//  TestingCarthage
+//
+//  Created by Adriana Pineda on 2/25/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestingCarthage : NSObject
+
+@end
---
+++ TestingCarthage/TestingCarthage.m
@@ -0,0 +1,13 @@
+//
+//  TestingCarthage.m
+//  TestingCarthage
+//
+//  Created by Adriana Pineda on 2/25/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+#import "TestingCarthage.h"
+
+@implementation TestingCarthage
+
+@end
