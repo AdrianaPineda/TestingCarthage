b904004cca692de3a3400ddd5cd9e9de17a7f6b6
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-02-25 18:50:54 -0500

    Add framework target

---
+++ TestingCarthageFramework/Sum.h
@@ -0,0 +1,14 @@
+//
+//  Sum.h
+//  TestingCarthage
+//
+//  Created by Adriana Pineda on 2/25/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sum : NSObject
+
+- (int)sumX:(int)x toZ:(int)z;
+@end
---
+++ TestingCarthageFramework/Sum.m
@@ -0,0 +1,16 @@
+//
+//  Sum.m
+//  TestingCarthage
+//
+//  Created by Adriana Pineda on 2/25/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+#import "Sum.h"
+
+@implementation Sum
+
+- (int)sumX:(int)x toZ:(int)z {
+    return 4;
+}
+@end
