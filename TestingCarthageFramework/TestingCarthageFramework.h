b144c748865444ca011c5c05540b522536e1fc3f
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-02-26 15:24:34 -0500

    Add NonPublicClass in TestingCarthageFramework target, otherwise it won't compile

--- TestingCarthageFramework/TestingCarthageFramework.h
+++ TestingCarthageFramework/TestingCarthageFramework.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char TestingCarthageFrameworkVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <TestingCarthageFramework/PublicHeader.h>
 
 #import <TestingCarthageFramework/Sum.h>
+#import <TestingCarthageFramework/TestingCarthage.h>
